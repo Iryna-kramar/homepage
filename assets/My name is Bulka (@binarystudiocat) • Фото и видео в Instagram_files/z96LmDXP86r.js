c6fe49de7e5388f3efaaf69aab86a81c839c7141
Iryna Kramar <irynakramar@Irynas-MacBook-Air.local>
2023-06-18 14:31:16 +0300

    Contact info items added

---
+++ assets/My name is Bulka (@binarystudiocat) • Фото и видео в Instagram_files/z96LmDXP86r.js
@@ -0,0 +1,3 @@
+;/*FB_PKG_DELIM*/
+
+__d("PolarisExploreLocationsRoot.entrypoint",["JSResourceForInteraction"],(function(a,b,c,d,e,f,g){"use strict";a={getPreloadProps:function(a){return{queries:{}}},root:c("JSResourceForInteraction")("PolarisExploreLocationsRoot.react").__setRef("PolarisExploreLocationsRoot.entrypoint")};g["default"]=a}),98);
